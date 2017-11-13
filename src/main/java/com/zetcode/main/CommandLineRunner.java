9366cc0e996fbdce0ada5422540bb81472281f57
Jan Bodnar <jan.bodnar@gmail.com>
2017-11-13 16:00:15 +0100

    Create CommandLineRunner.java

---
+++ src/main/java/com/zetcode/main/CommandLineRunner.java
@@ -0,0 +1,10 @@
+package com.zetcode.main;
+
+public class CommandLineRunner {
+
+    public static void main(String[] args) throws Exception {
+
+        JasperResourceBundle app = new JasperResourceBundle();
+        app.start();
+    }
+}
